06b4adc280880a9c3cb49754a3c149a826a03480
TeaJay <luxervice@gmail.com>
2016-04-15 22:06:46 +0600

    Fix Tests

--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -1,10 +1,10 @@
 package ru.javawebinar.topjava.service.datajpa;
 
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
 
 @ActiveProfiles(DATAJPA)
-public class DataJpaUserServiceTest extends AbstractUserServiceTest {
+public class DataJpaUserServiceTest extends AbstractJpaUserServiceTest {
 }
